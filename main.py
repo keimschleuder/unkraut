ac8ce3ace406b4d0d0b32536d9eb68e76e3297a1
Niklas <138971666+keimschleuder@users.noreply.github.com>
2025-07-09 11:39:22 +0200

    main.py aktualisieren

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import gpiod
 # Console Arguments init
 parser = argparse.ArgumentParser(prog = "main.py", 
                                  usage = "python main.py [-h]", 
-                                description = "Intended to run on a Raspberry Pi 5 with AI-HAT and AI-Camera. It detects harmful plants and destroys them.", 
+                                 description = "Intended to run on a Raspberry Pi 5 with AI-HAT and AI-Camera. It detects harmful plants and destroys them.", 
                                  epilog = "Program written for the 'Jugend Forscht'-Competition by Niklas Keim", 
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
