924e5c9f72dc18308bed3667d299081df4b290ba
jufo-nb <wasserspender.nbs@gmail.com>
2025-11-20 18:33:05 +0100

    cleaning things up

--- i2c.py
+++ i2c.py
@@ -1,15 +1,15 @@
 from smbus import SMBus
- 
-addr = 0x8 # bus address
+
+addr = 0x2f # bus address
 bus = SMBus(1) # indicates /dev/ic2-1
- 
+
 numb = 1
- 
+
 print ("Enter 1 for ON or 0 for OFF")
 while numb == 1:
- 
+
 	ledstate = input(">>>>   ")
- 
+
 	if ledstate == "1":
 		bus.write_byte(addr, 0x1) # switch it on
 	elif ledstate == "0":
