3d0f933011903879054b48b53a572f6331251a81
Niklas <138971666+keimschleuder@users.noreply.github.com>
2025-12-22 11:58:37 +0100

    small changes

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import rpi_drivers.servo
 parser = argparse.ArgumentParser(prog = "main.py", 
                                  usage = "python main.py [-h]", 
                                  description = "Intended to run on a Raspberry Pi 5 with AI-HAT and AI-Camera. It detects harmful plants and destroys them.", 
-                                 epilog = "Program written for the 'Jugend Forscht'-Competition by Niklas Keim", 
+                                 epilog = "Program written for the german Jugend Forscht-Competition by Niklas Keim", 
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 motor_drv = rpi_drivers.motor.Motor()
