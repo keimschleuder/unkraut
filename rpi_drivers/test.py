5679f5f09c51a6e73be8a634640d923f1d09aa22
Niklas <138971666+keimschleuder@users.noreply.github.com>
2025-12-28 15:58:22 +0100

    writing a test

--- rpi_drivers/test.py
+++ rpi_drivers/test.py
@@ -2,5 +2,22 @@ import rpi_drivers.arduino as arduino
 import plantnet
 import gpio
 import servo
+from time import sleep
 
 # TODO: Write a test for all modules used in the main Programm
+
+arduino_drv = arduino.Arduino()
+
+arduino_drv.motor(True)
+sleep(1)
+arduino_drv.pump(True)
+sleep(1)
+arduino_drv.motor(False)
+sleep(1)
+arduino_drv.motor(True)
+sleep(1)
+arduino_drv.pump(False)
+sleep(1)
+arduino_drv.pump(True)
+sleep(1)
+arduino_drv.halt()
